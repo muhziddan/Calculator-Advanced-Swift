476582f82fbaf98154a1d3c7c6ac373ca3a882c3
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2023-08-14 10:24:41 +0700

    Add numbers and operators IBAction with accumulative number function an AC function

--- CalculatorAdvanced/CalculatorAdvanced/ViewController.swift
+++ CalculatorAdvanced/CalculatorAdvanced/ViewController.swift
@@ -8,17 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var calculateLabel: UILabel!
+    var totalNumber: String = "0"
+    var comaPressedCount: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func calculatorButton(_ sender: Any) {
+    @IBAction func numberButtonPressed(_ sender: UIButton) {
         
+        guard let currentButtonText = sender.currentTitle else {return}
+        if currentButtonText == "." {
+            comaPressedCount += 1
+        }
         
+        if totalNumber == "0" && currentButtonText != "." {
+            totalNumber = currentButtonText
+        } else {
+            if currentButtonText == "." && comaPressedCount > 1 {return}
+            
+            totalNumber += currentButtonText
+        }
+        
+        calculateLabel.text = totalNumber
+    }
+    
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        
+        guard let currentButtonText = sender.currentTitle else {return}
+        
+        switch currentButtonText {
+            case "AC" :
+                totalNumber = "0"
+                calculateLabel.text = totalNumber
+                comaPressedCount = 0
+            
+//            case "/" :
+                
+            
+            default: print("safe")
+        }
     }
     
 }
-
-
